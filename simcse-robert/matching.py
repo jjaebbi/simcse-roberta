5a7de9a243af1f36907bfee99bfbaa51ac540961
umleeho1 <109669006+umleeho1@users.noreply.github.com>
2023-12-11 20:41:43 +0900

    matching complete not server

--- simcse-robert/matching.py
+++ simcse-robert/matching.py
@@ -1,56 +1,84 @@
-import pandas as pd
-import torch
+import pyodbc
 from transformers import AutoModel, AutoTokenizer
-from tqdm import tqdm
+import torch
+import nltk
+nltk.download('punkt')
+from nltk.tokenize import sent_tokenize
 
+# ... (calculate_similarity, get_highest_similarity_score 함수 정의)
 def calculate_similarity(model, tokenizer, text1, text2):
     inputs = tokenizer([text1, text2], return_tensors="pt", padding=True, truncation=True)
     with torch.no_grad():
         outputs = model(**inputs)
-        similarity_score = torch.nn.functional.cosine_similarity(outputs.last_hidden_state[0], outputs.last_hidden_state[1], dim=1)
+        similarity_scores = torch.nn.functional.cosine_similarity(outputs.last_hidden_state[0], outputs.last_hidden_state[1], dim=1)
+        similarity_score = similarity_scores.mean()  # 여러 점수의 평균을 계산
     return similarity_score
 
-def get_highest_similarity_score(model, tokenizer, text1, text2_list):
-    highest_score = -1.0  # 가장 높은 스코어를 저장할 변수 초기화
-    for text2 in text2_list:
-        similarity_score = calculate_similarity(model, tokenizer, text1, text2)
-        similarity_score = similarity_score[0].item()
-        if similarity_score > highest_score:
-            highest_score = similarity_score
-    return highest_score
 
-def create_eval_dataset(model_path, dataset_path):
-    tokenizer = AutoTokenizer.from_pretrained(model_path)
-    model = AutoModel.from_pretrained(model_path)
 
-    eval_examples = []
+def calculate_max_similarity_per_sentence(model, tokenizer, intro_text, job_posting_text):
+    intro_sentences = sent_tokenize(intro_text)
+    job_posting_sentences = sent_tokenize(job_posting_text)
 
-    dataset = pd.read_csv(dataset_path, delimiter="\t")
+    max_scores = []
+    for intro_sentence in intro_sentences:
+        sentence_scores = [
+            calculate_similarity(model, tokenizer, intro_sentence, job_posting_sentence).item()
+            for job_posting_sentence in job_posting_sentences
+        ]
+        max_scores.append(max(sentence_scores))
 
-    for _, row in tqdm(dataset.iterrows(), total=len(dataset)):
-        text1 = row["sentence1"]
-        text2_list = dataset["sentence2"].tolist()  # 모든 sentence2 문장 리스트로 변환
-        score = float(row["score"])
+    return sum(max_scores) / len(max_scores) if max_scores else 0
+# 데이터베이스 연결
+db = pyodbc.connect('DSN=aws_test_t6;UID=DAJOBA;PWD=DAJOBA')
+cursor = db.cursor()
 
-        highest_similarity_score = get_highest_similarity_score(model, tokenizer, text1, text2_list)
-        highest_similarity_score = round(highest_similarity_score, 2)  # 소수 둘째 자리 반올림
+# 트랜스포머 모델 및 토크나이저 초기화
+model_path = "kazma1/simcse-robertsmall-matching"
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = AutoModel.from_pretrained(model_path)
 
-        eval_examples.append({"sentence1": text1, "highest_score": highest_similarity_score, "true_score": score})
 
-    return pd.DataFrame(eval_examples)
 
-def main():
-    model_path = "kazma1/simcse-robertsmall-matching"  # 여기에 모델 경로를 지정하세요.
-    dataset_path = "data/dev/sts_dev.csv"  # 여기에 dev.csv 파일의 경로를 지정하세요.
 
-    eval_dataset = create_eval_dataset(model_path, dataset_path)
 
-    eval_dataset.to_csv("matchingresult.csv", index=False)
-    print(f"Eval dataset saved to matchingresult.csv")
 
-    # 평균 매칭률을 계산하여 출력
-    avg_matching_score = eval_dataset["highest_score"].mean()
-    print(f"Average Matching Score: {avg_matching_score:.2f}")
+while True:
+    cursor.execute("SELECT USER_ID, INTRO_ID, DESIRE_FIELD, INTRO_CONTENT FROM SELF_INTRODUCTION WHERE SIGNAL = 1")
+    row = cursor.fetchone()
 
-if __name__ == "__main__":
-    main()
+    if row is None:
+        print("All signals processed.")
+        break
+
+    intro_id = row[1]
+    desire_field = row[2]
+    intro_content = row[3]
+
+    # 해당 희망분야의 채용공고 불러오기
+    cursor.execute("SELECT JOB_POSTING_ID, TITLE, GROUP_INTRO, MAINDUTIES, QUALIFICATION, PREFERENTIAL FROM JOB_POSTING WHERE JOB_GROUP = ?", desire_field)
+    job_postings = cursor.fetchall()
+
+    # 매칭 점수 계산
+    scores = []
+    for posting in job_postings:
+        job_posting_id = posting[0]
+        job_posting_content = ' '.join(posting[1:])
+
+        max_similarity_score = calculate_max_similarity_per_sentence(model, tokenizer, intro_content,
+                                                                     job_posting_content)
+        scores.append((job_posting_id, max_similarity_score))
+
+    # 상위 5개 채용공고 선택
+    top_5_postings = sorted(scores, key=lambda x: x[1], reverse=True)[:5]
+
+    # MATCH 테이블에 결과 저장
+    for job_posting_id, score in top_5_postings:
+        cursor.execute("INSERT INTO MATCH (INTRO_ID, JOB_POSTINGID, MATCH-SCORE) VALUES (?, ?, ?)", (intro_id, job_posting_id, score))
+        db.commit()
+
+    # SIGNAL 값을 0으로 업데이트
+    cursor.execute("UPDATE SELF_INTRODUCTION SET SIGNAL = 0 WHERE INTRO_ID = ?", intro_id)
+    db.commit()
+
+print("Matching results saved to the MATCH table")
--- simcse-robert/tiberotest.py
+++ simcse-robert/tiberotest.py
@@ -1,45 +1,73 @@
-def print_hi(name):
- # Use a breakpoint in the code line below to debug your
 
-    print(f'Hi, {name}') # Press Ctrl+F8 to toggle the
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 # TIBERO DB
 import pyodbc
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import torch
+from transformers import AutoModel, AutoTokenizer
+from tqdm import tqdm
+
+
 db = pyodbc.connect('DSN=aws_test_t6;UID=DAJOBA;PWD=DAJOBA' )
 cursor = db.cursor()
-cursor.execute("SELECT COUNT(*) FROM EMP")
-for row in cursor:
-    totalemps = int(row[0])
-    print(totalemps)
-t = np.arange(totalemps)
-cursor.execute("select ename, sal from EMP")
-enames = []
-salaries = []
-for row in cursor:
-    totalemps = int(row[0])
-    print(totalemps)
-t = np.arange(totalemps)
-cursor.execute("select ename, sal from EMP")
-enames = []
-salaries = []
-for row in cursor:
-    enames.append(row[0])
-    salaries.append(row[1])
-bar_width = 0.5
-plt.bar(t, salaries, bar_width, label="Salary")
-plt.title("Employee Details")
-plt.xlabel("Employee")
-plt.ylabel("Salary")
-plt.xticks(t, enames)
-plt.grid(True)
-plt.legend()
-xs = [x for x in range(1, totalemps)]
-for x, y in zip(xs, salaries):
-    plt.annotate(salaries[x], (x - bar_width / 2, y))
-plt.show()
+# SIGNAL 값이 1인 레코드가 있을 때까지 반복
+#필요한 자소서 정보 가져오기
+
+
+
+
+while True:
+    cursor.execute("SELECT USER_ID,INTRO_ID,DESIRE_FIELD,INTRO_CONTENT FROM SELF_INTRODUCTION WHERE SIGNAL = 1")
+    row = cursor.fetchone() # 첫 번째 결과 가져오기
+
+    if row is None:
+        print("complete all signal")
+        break # 더 이상 SIGNAL이 1인 레코드가 없으면 반복 중단
+    # 각 레코드의 값을 사용하여 필요한 작업 수행
+    user_id = row[0]   # 유저ID type:number
+    intro_id = row[1]  # 자기소개서ID type:number
+    desire_field = row[2] #희망분야  type:numer
+    intro_cuntent = row[3]#자기소개서 type:CLOB
+
+    print("자기소개서 정보") #레코드 간 구분을 위한 빈 줄
+    print(f"User ID: {user_id}")
+    print(f"Intro ID: {intro_id}")
+    print(f"Desire Field: {desire_field}")
+    print(f"Intro Content: {intro_cuntent[:200]}")  # 자기소개서 내용이 길 경우, 처음 200자만 출력
+
+
+
+    # SIGNAL이 1인 레코드를 사용하여 필요한 작업 수행
+    # JOB_POSTING 테이블에서 현재 SELF_INTRODUCTION 레코드의 DESIRE_FIELD 값과 일치하는 레코드 찾기
+    query = "SELECT TITLE, GROUP_INTRO, MAINDUTIES, QUALIFICATION, PREFERENTIAL FROM JOB_POSTING WHERE JOB_GROUP = ?"
+    cursor.execute(query, (desire_field,))
+    matching_job_postings = cursor.fetchall()
+
+    # 각 레코드를 순회하면서 컬럼 값 추출
+    for job_posting in matching_job_postings:
+        job_title = job_posting[0]
+        group_intro = job_posting[1]
+        mainduties = job_posting[2]
+        qualification = job_posting[3]
+        preferential = job_posting[4]
+
+
+        # 각 레코드의 정보를 출력 또는 처리
+        print("JOB_POSTING")  # 레코드 간 구분을 위한 빈 줄
+        print(
+            f"Job Title: {job_title}, Group Intro: {group_intro}, Main Duties: {mainduties}, Qualification: {qualification}, Preferential: {preferential}")
+
+
+    # 필요한 처리 수행...
+
+
+
+    # 처리가 완료된 후 SIGNAL 값을 0으로 업데이트
+    cursor.execute("UPDATE SELF_INTRODUCTION SET SIGNAL = 0 WHERE USER_ID = ? AND INTRO_ID = ?", (user_id, intro_id))
+    db.commit()  # 변경 사항을 데이터베이스에 반영
+
+# 반복이 끝나면 필요한 후처리 수행
+cursor.execute("SELECT * FROM JOB_POSTING AS J,SELF_INTRODUCTION AS S WHERE J.JOB_GROUP = S.DESIRE_FIELD")
+
